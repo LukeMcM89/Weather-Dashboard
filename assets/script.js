6f3b62f43478b8685a1cc578c06a16337efe5e19
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-07-08 15:26:36 -0400

    API Key rework

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,29 @@
 const APIKEY = "ebbf326ee70a2c54d1a94e133f651689";
 
 var searchBtn = document.getElementById("btn");
-var zipInput = document.get ElementById("zipinput");
+var zipInput = document.getElementById("zipinput");
 
 var currentTemp = document.getElementById("temp");
 var currentWind = document.getElementById("wind");
 var currentHumid = document.getElementById("humidity");
 var currentUv = document.getElementById("uvindex");
 
-var dayOne = document.getElementById("day1")
+var dayOne = document.getElementById("day1");
+var dayTwo = document.getElementById("day2");
+var dayThree = document.getElementById("day3");
+var dayFour = document.getElementById("day4");
+var dayFive = document.getElementById("day5");
 
 
-
-var searchFunction = function() {
+var searchFunction = function(){
     let zipCode = zipInput.value;
     console.log(zipCode);
-    let apiAddress = “api.openweathermap.org/data/2.5/forecast?q={city name}&appid="ebbf326ee70a2c54d1a94e133f651689”;
+    let apiAddress = 'https://api.openweathermap.org/data/2.5/forecast?q=”+ zipCode + “&appid=” + APIKey';
     console.log(apiAddress);
-}
+    fetch(apiAddress)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+        console.log(data)})
+};
