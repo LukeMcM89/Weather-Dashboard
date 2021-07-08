8f943ea450b29a3a7e1337982ab4c49899501239
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-07-08 14:46:27 -0400

    Javascript Initials

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
-const APIKEY ="ebbf326ee70a2c54d1a94e133f651689";
+const APIKEY = "ebbf326ee70a2c54d1a94e133f651689";
+
+var searchBtn = document.getElementById("search");
+
+
+
+var searchFunction = function(){
+    let zipCode = zipInput.value;
+    console.log(zipCode);
+    let apiAddress = “api.openweathermap.org/data/2.5/forecast?q={city name}&appid="ebbf326ee70a2c54d1a94e133f651689”;
+    console.log(apiAddress);
+}
